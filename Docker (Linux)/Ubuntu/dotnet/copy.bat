98911ba7783d8d98febff2dae8e8e9bfe8898b5d
Carl <carlp@grindrodbank.co.za>
2018-03-30 09:32:30 +0200

    content fixes

--- Docker (Linux)/Ubuntu/dotnet/copy.bat
+++ Docker (Linux)/Ubuntu/dotnet/copy.bat
@@ -2,12 +2,12 @@ set PATH=C:\Program Files\PuTTY
 
 pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/appsettings.Development.json  carl@192.168.231.129:/home/carl/content
 pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/appsettings.json  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.deps.json  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.dll  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.pdb  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.PrecompiledViews.dll  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.PrecompiledViews.pdb  carl@192.168.231.129:/home/carl/content
-pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/WebApiCore2.runtimeconfig.json  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.deps.json  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.dll  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.pdb  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.PrecompiledViews.dll  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.PrecompiledViews.pdb  carl@192.168.231.129:/home/carl/content
+pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/aspnetapp.runtimeconfig.json  carl@192.168.231.129:/home/carl/content
 pscp c:/Dev-Code-School/DockerDemo/"Docker (Linux)"/Ubuntu/dotnet/content/web.config  carl@192.168.231.129:/home/carl/content
 
 echo "DONE"
