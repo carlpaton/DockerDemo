57d6b4ab0b95c0af76e3c6f2909f7acddbf03dbe
Carl <carlp@grindrodbank.co.za>
2018-04-04 21:56:10 +0200

    exec commands golden-gate bridge

--- Docker (Linux)/Ubuntu/user-defined-bridge-network/user-defined-bridge-network.sh
+++ Docker (Linux)/Ubuntu/user-defined-bridge-network/user-defined-bridge-network.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-#With its own bridge
+#links
 #https://docs.docker.com/network/network-tutorial-standalone/#use-user-defined-bridge-networks
+#https://app.pluralsight.com/player?course=docker-deep-dive-update&author=nigel-poulton&name=97b58d47-d08f-4894-88c7-4a7ec4c746ea&clip=1&mode=live
+
+#pull
+#sudo docker pull microsoft/dotnet:2.0-sdk
 
 #remove containers first, so the bridge can be removed
-sudo docker container kill postgres_nb #network bridge
-sudo docker rm postgres_nb
-sudo docker container kill dotnet-site_nb
-sudo docker rm dotnet-site_nb
+#_gg tells me its intended to be used on the 'golden-gate' bridge
+sudo docker container kill postgres_gg
+sudo docker rm postgres_gg
+sudo docker container kill dotnet_webapi_gg
+sudo docker rm dotnet_webapi_gg
 
-#Use user-defined bridge network
-sudo docker network rm postgres-net
-sudo docker network create --driver bridge postgres-net
+#Use user-defined bridge network called 'golden-gate' ~ lol with Nigel Poulton :D
+sudo docker network rm golden-gate 
+sudo docker network create --driver bridge golden-gate #'--driver bridge' is the default so can be omitted ~ can be 'overlay' to be scoped to the swarm
 
-#Psql container
-sudo docker run --name postgres_nb -p 5432:5432 -e "POSTGRES_PASSWORD=postgres" --network postgres-net -itd postgres:9.6-alpine
+#postgres container
+#adding the port map '-p 5432:5432' gives access to the image (on 5432) even when its on our 'golden-gate' bridge
+sudo docker run --name postgres_gg -e "POSTGRES_PASSWORD=postgres" --network golden-gate -itd postgres:9.6-alpine
 
-#dotnet-site_nb image
-sudo docker rmi image_name:dotnet-image_nb
-sudo docker build -t dotnet-image_nb .
+#dotnet image for our web api project
+sudo docker rmi image_name:dotnet_image_webapi
+sudo docker build -t dotnet_image_webapi .
 
-#dotnet-site_nb container (MVC Core Web API)
-sudo docker pull microsoft/dotnet:2.0-sdk
-sudo docker run --name dotnet-site_nb -p 192.168.231.129:80:80 --network postgres-net -itd dotnet-image_nb #network bridge 1
+#dotnet_webapi_gg container (MVC Core Web API)
+sudo docker run --name dotnet_webapi_gg -p 192.168.231.129:80:80 --network golden-gate -itd dotnet_image_webapi
 
 #display new containers & bridge
 sudo docker ps --all
 sudo docker network ls
 
-#option to add the container to user defined network after spinning it up??
-#sudo docker network connect bridge postgres_nb
+#option to add the container to user defined network after spinning it up
+#sudo docker network connect golden-gate [CONTAINER NAME]
+#sudo docker network connect bridge [CONTAINER NAME]
+
+#create EXEC session to container
+#sudo docker exec -it 279 sh (where 279 is the start of the container ID)
+#then you can PING [CONTAINER NAME] (note the container you ping needs to be on the same bridge, example golden-gate)
+#CRTL C ~ quit ping
+#CTRL PQ ~ quit EXEC session
+
+#inspect network
+#sudo docker network inspect [BRIDGE NAME]
