7c5fb36455996c444b88fa499695a68dbb497eb5
Carl <carl.paton@gmail.com>
2019-02-02 10:21:09 +1300

    paw patrol

---
+++ Docker For Windows/linux containers/mssql/run.ps1
@@ -0,0 +1,14 @@
+Try
+{
+    docker pull microsoft/mssql-server-linux:2017-CU13
+	docker container kill microsoftsqlserver_codegentool
+	docker rm microsoftsqlserver_codegentool
+	docker run --detach --name=microsoftsqlserver_codegentool -p 14331:1433  --env="ACCEPT_EULA=Y" --env="SA_PASSWORD=Password123" microsoft/mssql-server-linux:2017-CU13
+	docker start microsoftsqlserver_codegentool
+	docker ps --all	
+}
+Catch
+{
+   Write-Host "Exception!! ";
+   Write-Host $Error[0].Exception;
+}
